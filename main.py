6157496ddd8761c4961c9eb9d5f13f524a051336
lucas <lucas.pjensen10@gmail.com>
2023-12-31 11:01:04 -0800

    more guessing

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def connect_db():
     password = os.getenv("DB_PASS")
     database = os.getenv("DB_NAME")
 
-    if any([host, user, password, database]) is None:
+    if None in (host, user, password, database):
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="err reading env vars",
@@ -30,9 +30,16 @@ def connect_db():
 
     print(host, user, password, database)
 
-    return mysql.connector.connect(
-        host=host, user=user, password=password, database=database
-    )
+    try:
+        return mysql.connector.connect(
+            host=host, user=user, password=password, database=database
+        )
+    except Exception as e:
+        print(f"err connecting to db: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="err connecting to db",
+        )
 
 
 app = FastAPI()
