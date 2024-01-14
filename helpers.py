8b9b3faeba0c5b2dae021c6a64c8f7a190ca4848
Lucas Jensen <lucas.p.jensen10@gmail.com>
2024-01-13 17:49:37 -0800

    added domain to origins

--- helpers.py
+++ helpers.py
@@ -4,11 +4,9 @@ from dotenv import load_dotenv
 
 origins = [
     "http://localhost",
-    "https://localhost",
-    "http://localhost:8000",
-    "https://localhost:8000",
     "http://localhost:3000",
     "https://localhost:3000",
+    "https://lucasjensen.me/"
 ]
 
 
